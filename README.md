f0a536500185035dde332b62891d4756cdd19120
Markus Digruber <mdigruber@MarkusBook.local>
2022-01-14 11:56:47 +0100

    added Readme and improved code

---
+++ README.md
@@ -0,0 +1,11 @@
+# Microbrowser
+
+A small and minimalistic browser written in Electron. It uses the Chromium Browser to render Websites. Very easy to use.
+
+## Installation:
+Clone the git repo, install the npm packages and start it.
+
+- git clone https://www.github.com/mdigruber/microbrowser
+- cd microbrowser
+- npm install
+- npm start
--- renderer.js
+++ renderer.js
@@ -1,43 +1,63 @@
-var browser = document.getElementById("view");
+class Browser {
 
-document.getElementById('searchtext').addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    searchURL();
+  constructor() {
+    this.browser = document.getElementById("view")
+    this.browserTitle = document.querySelector("title").innerHTML
   }
-})
+
+  searchfield() {
+    document.getElementById('searchtext').addEventListener('keypress', e => {
+      if (e.key === 'Enter') {
+        var text = document.getElementById("searchtext")
+        this.searchURL(text)
+      }
+    })
+  }
+
+  undoPage() {
+    if(this.browser.canGoBack()){
+      return this.browser.goBack()
+    }
+  }
+
+  redoPage() {
+    if(this.browser.canGoForward()){
+       return this.browser.goForward()
+    }
+  }
+
+  reloadPage() {
+    return this.browser.reload()
+  }
+
+  // if search is a valid URL, then the browser will directly open the URL
+  // if the search consists of word, the browser will open a new google search
+  searchURL(text) {
+    if(text.value.startsWith("http:") || text.value.startsWith("https:")){
+      this.browser.loadURL(text.value)
+    } else {
+      this.browser.loadURL("https://www.google.com/search?q=" + text.value)
+    }
+    text.innerHTML = document.location.href
+    this.browserTitle = this.browser.getTitle()
+  }
+}
+
+var browser = new Browser()
+browser.searchfield()
 
 document.getElementById("undo").addEventListener("click", () => {
-  if(browser.canGoBack()){
-    browser.goBack();
-  }
+  browser.undoPage()  
 })
 
 document.getElementById("redo").addEventListener("click",  async () => {
-  if(browser.canGoForward()){
-    await browser.goForward();
-  }
+  browser.redoPage()
 })
 
 document.getElementById("reload").addEventListener("click", () => {
-  browser.reload()
+  browser.reloadPage()
 })
 
 
-function searchURL() {
-  var text = document.getElementById("searchtext");
 
-  if(text.value.startsWith("http:") || text.value.startsWith("https:")){
-    browser.loadURL(text.value)
-  } else {
-    browser.loadURL("https://www.google.com/search?q=" + text.value)
-  }
-  text.innerHTML = document.location.href
-  
-  document.querySelector("title").innerHTML = browser.getTitle()
-}
 
-browser.addEventListener("click", () => {
-  console.log("hallo")
-  var text = document.getElementById("searchtext");
-  text.innerHTML = browser.src
-})
