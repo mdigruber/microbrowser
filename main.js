7857f1105f9be083c117fc481782d3cca3981cb7
Markus Digruber <mdigruber@MarkusBook.local>
2022-01-14 15:47:59 +0100

    improvements and icon changes

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
-const { app, BrowserWindow, ipcMain, nativeTheme, BrowserView } = require('electron')
+const { app, BrowserWindow, TouchBar, nativeTheme, BrowserView, Notification } = require('electron')
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar
 const path = require('path')
+
+const touchbtn = new TouchBar({
+  items : [
+    new TouchBarButton({label: "🤖", click: () => {
+      console.log("Beep Boop")
+    }}),
+    new TouchBarSpacer({ size: 'large' }),
+  ]
+})
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 1000,
     webPreferences: {
-     
+      spellcheck: true,
       webviewTag: true, 
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
   win.loadFile('index.html')
+  win.setTouchBar(touchbtn)
+}
 
-
-  // ipcMain.handle('dark-mode:toggle', () => {
-  //   console.log("hallo")
-  // })
-
-  // ipcMain.handle('dark-mode:system', () => {
-  //   nativeTheme.themeSource = 'system'
-  // })
+function showNotification () {
+  new Notification({ title: "Welcome to Microbrowser", body: "Thanks for activating the Notifications" }).show()
 }
 
 app.whenReady().then(() => {
   createWindow()
+}).then(showNotification)
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
 
 app.on('window-all-closed', () => {
